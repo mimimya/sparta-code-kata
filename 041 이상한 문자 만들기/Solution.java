8781f3a768431686f71eb2907d11d39df8796d32
SEOUNJU PARK <sj.ella1030@gmail.com>
2026-03-19 10:59:00 +0900

    2026.03.19

---
+++ 041 이상한 문자 만들기/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        int idx = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == ' ') {
+                idx = 0;
+                sb.append(c);
+            } else {
+                sb.append(idx++ % 2 == 0 
+                    ? Character.toUpperCase(c) 
+                    : Character.toLowerCase(c));
+            }
+        }
+
+        return sb.toString();
+    }
+}
